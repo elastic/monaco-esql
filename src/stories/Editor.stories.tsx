db1f6cf1642325b19ad216626d68a8fd6ce99344
vadimkibana <vadimkibana@gmail.com>
2025-03-03 15:29:01 +0100

    setup storybook

---
+++ src/stories/Editor.stories.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import type {Meta, StoryObj} from '@storybook/react';
+
+const meta: Meta = {
+  title: 'Editor',
+};
+
+export default meta;
+
+export const Primary: StoryObj<typeof meta> = {
+  render: () => <div>asdf</div>,
+};
