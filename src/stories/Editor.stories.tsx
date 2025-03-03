834bc8edbc4ca9d43e3606f3801269f9c50735ed
vadimkibana <vadimkibana@gmail.com>
2025-03-03 18:50:01 +0100

    setup monaco editor story

--- src/stories/Editor.stories.tsx
+++ src/stories/Editor.stories.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import type {Meta, StoryObj} from '@storybook/react';
+import Editor from '@monaco-editor/react';
+import example from '../../example.esql?raw';
+import {language} from '../monarch';
 
 const meta: Meta = {
   title: 'Editor',
@@ -8,5 +11,19 @@ const meta: Meta = {
 export default meta;
 
 export const Primary: StoryObj<typeof meta> = {
-  render: () => <div>asdf</div>,
+  render: () => (
+    <div>
+      <Editor
+        height="90vh"
+        defaultLanguage='esql'
+        language='esql'
+        defaultValue={example}
+        onMount={(_, monaco) => {
+          console.log('monaco', monaco);
+          monaco.languages.register({id: 'esql'});
+          monaco.languages.setMonarchTokensProvider('esql', language);
+        }}
+      />
+    </div>
+  ),
 };
