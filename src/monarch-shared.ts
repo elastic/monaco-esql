1028b88560371d93963e6644e88367b6cb7e0994
vadimkibana <vadimkibana@gmail.com>
2025-04-14 14:49:47 +0200

    make monarch grammar dependencies injectable

---
+++ src/monarch-shared.ts
@@ -0,0 +1,4 @@
+import { create } from "./monarch";
+import * as definitions from "./definitions";
+
+export const language = create(definitions);
--- src/monarch.ts
+++ src/monarch.ts
@@ -1,394 +1,194 @@
 import type { languages } from "monaco-editor";
 
-const commands = [
-	"DISSECT",
-	"DROP",
-	"ENRICH",
-	"EVAL",
-	"EXPLAIN",
-	"FORK",
-	"FROM",
-	"FULL JOIN",
-	"GROK",
-	"INFO",
-	"INLINESTATS",
-	"JOIN",
-	"KEEP",
-	"LEFT JOIN",
-	"LEFT",
-	"LIMIT",
-	"LOOKUP JOIN",
-	"LOOKUP",
-	"METRICS",
-	"MV_EXPAND",
-	"RENAME",
-	"RIGHT JOIN",
-	"RIGHT",
-	"ROW",
-	"SHOW INFO",
-	"SHOW",
-	"SORT",
-	"STATS",
-	"WHERE",
-];
+export type CreateDependencies = Partial<typeof import("./definitions")>;
 
-const options = ["BY", "ON", "WITH", "METADATA", "WHERE"];
+export const create = (
+	deps: CreateDependencies = {},
+): languages.IMonarchLanguage => {
+	const {
+		commands = [],
+		options = [],
+		literals = [],
+		functions = [],
+		delimiters = [],
+	} = deps;
 
-const literals = ["TRUE", "FALSE", "NULL"];
+	return {
+		// Uncomment when developing.
+		// defaultToken: "invalid",
 
-const functions = [
-	"ABS",
-	"ACOS",
-	"ASIN",
-	"ATAN",
-	"ATAN2",
-	"AVG",
-	"BIT_LENGTH",
-	"BUCKET",
-	"BYTE_LENGTH",
-	"CASE",
-	"CATEGORIZE",
-	"CBRT",
-	"CEIL",
-	"CIDR_MATCH",
-	"COALESCE",
-	"CONCAT",
-	"COS",
-	"COSH",
-	"COUNT_DISTINCT",
-	"COUNT",
-	"DATE_DIFF",
-	"DATE_EXTRACT",
-	"DATE_FORMAT",
-	"DATE_PARSE",
-	"DATE_TRUNC",
-	"E",
-	"ENDS_WITH",
-	"EXP",
-	"FLOOR",
-	"FROM_BASE64",
-	"GREATEST",
-	"HASH",
-	"HYPOT",
-	"IP_PREFIX",
-	"LEAST",
-	"LEFT",
-	"LENGTH",
-	"LOCATE",
-	"LOG",
-	"LOG10",
-	"LTRIM",
-	"MATCH",
-	"MAX",
-	"MEDIAN_ABSOLUTE_DEVIATION",
-	"MEDIAN",
-	"MIN",
-	"MV_APPEND",
-	"MV_AVG",
-	"MV_CONCAT",
-	"MV_COUNT",
-	"MV_DEDUPE",
-	"MV_FIRST",
-	"MV_LAST",
-	"MV_MAX",
-	"MV_MEDIAN_ABSOLUTE_DEVIATION",
-	"MV_MEDIAN",
-	"MV_MIN",
-	"MV_PERCENTILE",
-	"MV_PSERIES_WEIGHTED_SUM",
-	"MV_SLICE",
-	"MV_SORT",
-	"MV_SUM",
-	"MV_ZIP",
-	"NOW",
-	"PERCENTILE",
-	"PI",
-	"POW",
-	"QSTR",
-	"REPEAT",
-	"REPLACE",
-	"REVERSE",
-	"RIGHT",
-	"ROUND",
-	"RTRIM",
-	"SIGNUM",
-	"SIN",
-	"SINH",
-	"SPACE",
-	"SPLIT",
-	"SQRT",
-	"ST_CENTROID_AGG",
-	"ST_CONTAINS",
-	"ST_DISJOINT",
-	"ST_DISTANCE",
-	"ST_ENVELOPE",
-	"ST_EXTENT_AGG",
-	"ST_INTERSECTS",
-	"ST_WITHIN",
-	"ST_X",
-	"ST_XMAX",
-	"ST_XMIN",
-	"ST_Y",
-	"ST_YMAX",
-	"ST_YMIN",
-	"STARTS_WITH",
-	"STD_DEV",
-	"SUBSTRING",
-	"SUM",
-	"TAN",
-	"TANH",
-	"TAU",
-	"TO_BASE64",
-	"TO_BOOLEAN",
-	"TO_CARTESIANPOINT",
-	"TO_CARTESIANSHAPE",
-	"TO_DATE_NANOS",
-	"TO_DATEPERIOD",
-	"TO_DATETIME",
-	"TO_DEGREES",
-	"TO_DOUBLE",
-	"TO_GEOPOINT",
-	"TO_GEOSHAPE",
-	"TO_INTEGER",
-	"TO_IP",
-	"TO_LONG",
-	"TO_LOWER",
-	"TO_RADIANS",
-	"TO_STRING",
-	"TO_TIMEDURATION",
-	"TO_UNSIGNED_LONG",
-	"TO_UPPER",
-	"TO_VERSION",
-	"TOP",
-	"TRIM",
-	"VALUES",
-	"WEIGHTED_AVG",
-];
+		// ES|QL is case-insensitive.
+		ignoreCase: true,
 
-const delimiters = [
-	"/",
-	".",
-	",",
-	"=~",
-	"<=",
-	">=",
-	"==",
-	"!=",
-	"===",
-	"!==",
-	"=>",
-	"+",
-	"-",
-	"**",
-	"*",
-	"/",
-	"%",
-	"++",
-	"--",
-	"<<",
-	"</",
-	">>",
-	">>>",
-	"&",
-	"|",
-	"^",
-	"!",
-	"~",
-	"&&",
-	"||",
-	"?",
-	":",
-	"=",
-	"+=",
-	"-=",
-	"*=",
-	"**=",
-	"/=",
-	"%=",
-	"<<=",
-	">>=",
-	">>>=",
-	"&=",
-	"|=",
-	"^=",
-	"@",
-];
+		// Lists of known language keywords and built-ins.
+		commands,
+		options,
+		literals,
+		functions,
+		delimiters,
+		namedOperators: [
+			...(deps.operators?.named?.binary ?? []),
+			...(deps.operators?.named?.other ?? []),
+		],
 
-const binaryNamedOperators = ["AND", "OR", "IS", "IN", "AS", "LIKE", "RLIKE"];
-const otherNamedOperators = ["ASC", "DESC", "FIRST", "LAST", "NULLS", "NOT"];
+		// Pre-defined regular expressions.
+		escapes:
+			/\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+		digits: /\d+(_+\d+)*/,
 
-export const monarchLanguage: languages.IMonarchLanguage = {
-	// Uncomment when developing.
-	// defaultToken: "invalid",
+		brackets: [
+			{ open: "[", close: "]", token: "delimiter.square" },
+			{ open: "(", close: ")", token: "delimiter.parenthesis" },
+			{ open: "{", close: "}", token: "delimiter.curly" },
+			{ open: "<", close: ">", token: "delimiter.angle" },
+		],
 
-	// ES|QL is case-insensitive.
-	ignoreCase: true,
+		tokenizer: {
+			root: [
+				{ include: "@whitespace" },
 
-	// Lists of known language keywords and built-ins.
-	commands,
-	options,
-	literals,
-	functions,
-	delimiters,
-	namedOperators: [...binaryNamedOperators, ...otherNamedOperators],
-
-	// Pre-defined regular expressions.
-	escapes:
-		/\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
-	digits: /\d+(_+\d+)*/,
-
-	brackets: [
-		{ open: "[", close: "]", token: "delimiter.square" },
-		{ open: "(", close: ")", token: "delimiter.parenthesis" },
-		{ open: "{", close: "}", token: "delimiter.curly" },
-		{ open: "<", close: ">", token: "delimiter.angle" },
-	],
-
-	tokenizer: {
-		root: [
-			{ include: "@whitespace" },
-
-			// Keywords
-			[
-				/[a-zA-Z_$][\w$]*/,
-				{
-					cases: {
-						"@commands": { token: "keyword.command.$0" },
-						"@options": { token: "keyword.option.$0" },
-						"@literals": { token: "keyword.literal.$0" },
-						"@functions": { token: "identifier.function.$0" },
-						"@namedOperators": { token: "keyword.operator.$0" },
-						"@default": "identifier",
+				// Keywords
+				[
+					/[a-zA-Z_$][\w$]*/,
+					{
+						cases: {
+							"@commands": { token: "keyword.command.$0" },
+							"@options": { token: "keyword.option.$0" },
+							"@literals": { token: "keyword.literal.$0" },
+							"@functions": { token: "identifier.function.$0" },
+							"@namedOperators": { token: "keyword.operator.$0" },
+							"@default": "identifier",
+						},
 					},
-				},
-			],
-			
-			{ include: "@expression" },
-			{ include: "@processingCommand" },
+				],
 
-			[/\[|\(|\)|\]/, "@brackets"],
-			[
-				/[^\s]+/,
-				{
-					cases: {
-						"@delimiters": "delimiter",
+				{ include: "@expression" },
+				{ include: "@processingCommand" },
+
+				[/\[|\(|\)|\]/, "@brackets"],
+				[
+					/[^\s]+/,
+					{
+						cases: {
+							"@delimiters": "delimiter",
+						},
 					},
-				},
-			],
-		],
-
-		// --------------------------------- Hidden channel: whitespace and comments
-
-		whitespace: [
-			[/[ \t\r\n]+/, ""],
-			[/\/\*\*(?!\/)/, "comment.doc", "@doc"],
-			[/\/\*/, "comment", "@comment"],
-			[/\/\/.*$/, "comment"],
-		],
-
-		comment: [
-			[/[^\/*]+/, "comment"],
-			[/\*\//, "comment", "@pop"],
-			[/[\/*]/, "comment"],
-		],
-
-		doc: [
-			[/[^\/*]+/, "comment.doc"],
-			[/\*\//, "comment.doc", "@pop"],
-			[/[\/*]/, "comment.doc"],
-		],
-
-		// ---------------------------------------------------------------- Commands
-
-		// This code block allows to color commands when they are followed by a pipe
-		// character "|", this way all new processing commands are color even if
-		// they are not part of the command name list.
-		processingCommand: [
-			[
-				/\|/,
-				{ token: "delimiter.pipe", switchTo: "@beforeMnemonicWhitespace" },
-			],
-		],
-
-		beforeMnemonicWhitespace: [
-			{ include: "@whitespace" },
-			["", { token: "", switchTo: "@commandName" }],
-		],
-
-		// Matches *command name*, i.e. the mnemonic.
-		commandName: [
-			// First tries to match all known command names.
-			[
-				commands.join("|"),
-				{ token: "keyword.command.name", switchTo: "@root" },
+				],
 			],
 
-			// If command name is not well known, just matches the first word.
-			[/\w+\b/, { token: "keyword.command.name", switchTo: "@root" }],
-		],
+			// --------------------------------- Hidden channel: whitespace and comments
 
-		// ------------------------------------------------------------- Expressions
+			whitespace: [
+				[/[ \t\r\n]+/, ""],
+				[/\/\*\*(?!\/)/, "comment.doc", "@doc"],
+				[/\/\*/, "comment", "@comment"],
+				[/\/\/.*$/, "comment"],
+			],
 
-		expression: [
-			{ include: "@whitespace" },
-			{ include: "@literal" },
+			comment: [
+				[/[^\/*]+/, "comment"],
+				[/\*\//, "comment", "@pop"],
+				[/[\/*]/, "comment"],
+			],
 
-			// Inline casts: 1.3::INTEGER
-			[/::\w+\b/, "type"],
+			doc: [
+				[/[^\/*]+/, "comment.doc"],
+				[/\*\//, "comment.doc", "@pop"],
+				[/[\/*]/, "comment.doc"],
+			],
 
-			// strings
-			[/"""/, "string.triple", "@string_triple"],
-			[/"([^"\\]|\\.)*$/, "string.invalid"], // non-terminated string
-			[/'([^'\\]|\\.)*$/, "string.invalid"], // non-terminated string
-			[/"/, "string", "@string"],
+			// ---------------------------------------------------------------- Commands
 
-			// Escaped column parts: nested.`escaped`.column
-			[/`/, "string", "@column_escape_part"],
-		],
+			// This code block allows to color commands when they are followed by a pipe
+			// character "|", this way all new processing commands are color even if
+			// they are not part of the command name list.
+			processingCommand: [
+				[
+					/\|/,
+					{ token: "delimiter.pipe", switchTo: "@beforeMnemonicWhitespace" },
+				],
+			],
 
-		literal: [
-			{ include: "@number" },
+			beforeMnemonicWhitespace: [
+				{ include: "@whitespace" },
+				["", { token: "", switchTo: "@commandName" }],
+			],
 
-			// Params
-			[
-				/\?{1,9}(([a-zA-Z_][a-zA-Z_0-9]+)|[0-9]+)?/,
-				{
-					cases: {
-						"\\?{1,9}": "variable.name.unnamed",
-						"\\?{1,9}([a-zA-Z_][a-zA-Z_0-9]+)?": "variable.name.named",
-						"\\?{1,9}([0-9]+)?": "variable.name.positional",
+			// Matches *command name*, i.e. the mnemonic.
+			commandName: [
+				// First tries to match all known command names.
+				[
+					commands.join("|"),
+					{ token: "keyword.command.name", switchTo: "@root" },
+				],
+
+				// If command name is not well known, just matches the first word.
+				[/\w+\b/, { token: "keyword.command.name", switchTo: "@root" }],
+			],
+
+			// ------------------------------------------------------------- Expressions
+
+			expression: [
+				{ include: "@whitespace" },
+				{ include: "@literal" },
+
+				// Inline casts: 1.3::INTEGER
+				[/::\w+\b/, "type"],
+
+				// strings
+				[/"""/, "string.triple", "@string_triple"],
+				[/"([^"\\]|\\.)*$/, "string.invalid"], // non-terminated string
+				[/'([^'\\]|\\.)*$/, "string.invalid"], // non-terminated string
+				[/"/, "string", "@string"],
+
+				// Escaped column parts: nested.`escaped`.column
+				[/`/, "string", "@column_escape_part"],
+			],
+
+			literal: [
+				{ include: "@number" },
+
+				// Params
+				[
+					/\?{1,9}(([a-zA-Z_][a-zA-Z_0-9]+)|[0-9]+)?/,
+					{
+						cases: {
+							"\\?{1,9}": "variable.name.unnamed",
+							"\\?{1,9}([a-zA-Z_][a-zA-Z_0-9]+)?": "variable.name.named",
+							"\\?{1,9}([0-9]+)?": "variable.name.positional",
+						},
 					},
-				},
+				],
 			],
-		],
 
-		number: [
-			[/(@digits)[eE]([\-+]?(@digits))?/, "number.float"],
-			[/(@digits)\.(@digits)([eE][\-+]?(@digits))?/, "number.float"],
-			[/(@digits)/, "number"],
-		],
+			number: [
+				[/(@digits)[eE]([\-+]?(@digits))?/, "number.float"],
+				[/(@digits)\.(@digits)([eE][\-+]?(@digits))?/, "number.float"],
+				[/(@digits)/, "number"],
+			],
 
-		// Single double-quote strings: "str"
-		string: [
-			[/[^\\"]+/, "string"],
-			[/@escapes/, "string.escape"],
-			[/\\./, "string.escape.invalid"],
-			[/"/, "string", "@pop"],
-		],
+			// Single double-quote strings: "str"
+			string: [
+				[/[^\\"]+/, "string"],
+				[/@escapes/, "string.escape"],
+				[/\\./, "string.escape.invalid"],
+				[/"/, "string", "@pop"],
+			],
 
-		// Triple double-quoted strings: """str"""
-		string_triple: [
-			[/"""/, "string.triple", "@pop"],
-			[/[^"]+/, "string.triple"],
-		],
+			// Triple double-quoted strings: """str"""
+			string_triple: [
+				[/"""/, "string.triple", "@pop"],
+				[/[^"]+/, "string.triple"],
+			],
 
-		// Backtick quoted "strings". ES|QL does not have back-tick "strings"
-		// *per se*, but column parts can have backtick-escaped parts.
-		column_escape_part: [
-			[/[^`]+/, "string"],
-			[/@escapes/, "string.escape"],
-			[/\\./, "string.escape.invalid"],
-			[/`/, "string", "@pop"],
-		],
-	},
+			// Backtick quoted "strings". ES|QL does not have back-tick "strings"
+			// *per se*, but column parts can have backtick-escaped parts.
+			column_escape_part: [
+				[/[^`]+/, "string"],
+				[/@escapes/, "string.escape"],
+				[/\\./, "string.escape.invalid"],
+				[/`/, "string", "@pop"],
+			],
+		},
+	};
 };
