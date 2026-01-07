d0991e992ed8f8df9f66a51c6ac1e30485aecffa
Sebastian Delle Donne <sebastian.delledonne@elastic.co>
2026-01-07 16:04:04 +0100

    [ES|QL] Add licence to src files (#125)
    * add NOTICE.txt
    
    * better name
    
    * Add licence check to linter
    
    * Add checks to execute before merging
    
    * update licence text
    
    * clean
    
    * use fixed versions

--- src/stories/Editor.stories.tsx
+++ src/stories/Editor.stories.tsx
@@ -1,4 +1,12 @@
-// biome-ignore lint: React is used in JSX
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch B.V. licenses this file to you under
+ * the MIT license (the "License"); you may
+ * not use this file except in compliance with the License.
+ */
+
 import * as React from "react";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import Editor from "@monaco-editor/react";
